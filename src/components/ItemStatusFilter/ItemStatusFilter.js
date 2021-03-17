844fbce9b8affe55c4256895a0596e69f050fb3f
FeroLux <aleks.ferolux@gmail.com>
2021-03-17 22:31:47 +0500

    Реализовал логику добавления класса на кнопку при активном состоянии.

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "./itemStatusFilter.css";
 
-const ItemStatusFilter = ({changeTodoFilter}) => {
+const ItemStatusFilter = ({changeTodoFilter, filterId}) => {
     const onTodoFilterClick = (filterId) => {
         changeTodoFilter(filterId);
     }
+
+    const filterArr = [
+        {filterId: 1, filterText: "All"},
+        {filterId: 2, filterText: "Active"},
+        {filterId: 3, filterText: "Done"}
+        ];
+
+    const buttons = filterArr.map(el => {
+        let buttonClassName = "item-status-filter__button";
+        if(el.filterId === filterId) {
+            buttonClassName += " item-status-filter__button--active"
+        }
+        return (
+            <button key={el.filterId} onClick={() => onTodoFilterClick(el.filterId)}
+                className={buttonClassName}>{el.filterText}</button>
+        )
+    })
+
+
     return (
         <div className="item-status-filter">
-            <button onClick={() => onTodoFilterClick(1)} className="item-status-filter__button item-status-filter__button--active">All</button>
-            <button onClick={() => onTodoFilterClick(2)} className="item-status-filter__button ">Active</button>
-            <button onClick={() => onTodoFilterClick(3)} className="item-status-filter__button ">Done</button>
+            {buttons}
         </div>
     )
 }
