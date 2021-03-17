13920fac0e1cb170ad65f79359b9797acc5b850e
FeroLux <aleks.ferolux@gmail.com>
2021-03-17 15:58:33 +0500

    Добавил обработчики события на кнопки, для изменения статуса задачи.

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,17 +3,25 @@ import TodoItem from "../TodoItem";
 
 import "./TodoList.css";
 
-const TodoList = ({todoData}) => {
+const TodoList = (props) => {
+
+    const {todoData, updateStatus} = props;
 
     const elements = todoData.map(item => {
+
+        if(item.statuses[2].status) {
+            return null;
+        }
+
         const {id, ...itemProps} = item;
+
         return (
             <li className="todo-item" key={id}>
                 <TodoItem {...itemProps}/>
                 <div className="todo-item__buttons">
-                    <button className="todo-item-button todo-item-button--active todo-item-button--ok">OK</button>
-                    <button className="todo-item-button todo-item-button--del">DEL</button>
-                    <button className="todo-item-button todo-item-button--i">!</button>
+                    <button onClick={() => updateStatus(id, 1)} className="todo-item-button todo-item-button--active todo-item-button--ok">OK</button>
+                    <button onClick={() => updateStatus(id, 2)} className="todo-item-button todo-item-button--del">DEL</button>
+                    <button onClick={() => updateStatus(id, 3)} className="todo-item-button todo-item-button--i">!</button>
                 </div>
             </li>
         )
